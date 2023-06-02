fbd7a2467281cef8207f4649f96d5c08e2909bdd
ZokirjonovaMuslima <muslimazokirjonova2004@gmail.com>
2023-06-02 16:23:48 +0500

    update

--- root/swagger.py
+++ root/swagger.py
@@ -5,7 +5,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="Kun.uz API",
         default_version='v1',
         description="Test description",
         terms_of_service="https://www.google.com/policies/terms/",
